f902d70facc129295d2f5542c2d0c010de273e7f
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-03-28 16:43:33 -0300

    Criando a parte funcional para a rota /api/:data

---
+++ frontend/client/src/components/TimeStamp.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ShowTime from "./ShowTime";
+import TimeStampForm from "./TimeStampForm";
+
+const TimeStamp = () => {
+    const [utc, setUtc] = useState('')
+    const [unix, setUnix] = useState('')
+    const [withTimezone, setWithTimezone] = useState('')
+
+    const handleSubmit = async (dataInput: string, timezoneInput: string) => {
+        const params = new URLSearchParams();
+        params.append('timezone', timezoneInput || 'UTC');
+
+        const response = await fetch(`http://127.0.0.1:3000/api/${dataInput || ""}?` + params.toString(), {
+            method: 'GET',
+        });
+
+        const data = await response.json()
+        setUtc(data.utc)
+        setUnix(data.unix)
+        setWithTimezone(data.formatedTime)
+    };
+
+    return (
+        <section>
+            <TimeStampForm onSubmit={handleSubmit} />
+            <section>
+                <h2>Tempos</h2>
+                <ShowTime label="UTC" content={utc}></ShowTime>
+                <ShowTime label="UNIX" content={unix}></ShowTime>
+                <ShowTime label="Com timezone" content={withTimezone}></ShowTime>
+            </section>
+        </section>
+        
+    );
+};
+
+export default TimeStamp;
